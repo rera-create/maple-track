10373d6f994ced2e8ce838310352436eb9feef31
rera-create <itskainotchi@gmail.com>
2026-02-27 14:45:09 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -442,7 +442,7 @@ export default function App() {
     const r = parseInt(hex.slice(1,3),16);
     const g = parseInt(hex.slice(3,5),16);
     const b = parseInt(hex.slice(5,7),16);
-    return \`\${r},\${g},\${b}\`;
+    return `${r},${g},${b}`;
   };
 
   // Is the background dark? Used for text contrast
